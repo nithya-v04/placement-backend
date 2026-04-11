9a179cdbc5e189f755120a5c7a0dcadeb0b0ca74
Nithya Vaddepally <nithyavaddepally11@gmail.com>
2026-04-11 18:37:16 +0530

    added cors config

---
+++ src/main/java/com/placement/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.placement.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://placement-frontend-three-alpha.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
